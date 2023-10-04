a35e916be98864d13bf74069234a4d98768cc22e
Yousra Chakib <yochakib@e3r7p18.1337.ma>
2023-10-04 12:18:06 +0100

    done

--- ex01/contact.cpp
+++ ex01/contact.cpp
@@ -1,31 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/01 01:30:15 by yochakib          #+#    #+#             */
-/*   Updated: 2023/10/03 20:29:33 by yochakib         ###   ########.fr       */
+/*   Created: 2023/10/04 12:12:04 by yochakib          #+#    #+#             */
+/*   Updated: 2023/10/04 12:15:57 by yochakib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream> // std in std output operation
-#include <string> // string manipulation
+#include "contact.hpp"
 
-int main(int ac, char **av)
-{
-	if (ac < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		for (int i = 1; av[i]; i++)
-		{
-			for (int j = 0; av[i][j]; j++)
-				std::cout << (char)toupper(av[i][j]);
-				
-		}
-	}
-	std::cout << std::endl;
-	return (0);
-}
+// Constructor definition
+Contact::Contact(const std::string& firstName, const std::string& lastName, const std::string& nickname,
+                 const std::string& phoneNumber, const std::string& darkestSecret)
+    : firstName(firstName), lastName(lastName), nickname(nickname),
+      phoneNumber(phoneNumber), darkestSecret(darkestSecret) {}
---
+++ ex01/contact.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 11:42:26 by yochakib          #+#    #+#             */
+/*   Updated: 2023/10/04 12:11:53 by yochakib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+
+public:
+    // Constructor
+    Contact();
+
+    // Getter methods
+    std::string getFirstName() const { return firstName; }
+    std::string getLastName() const { return lastName; }
+    std::string getNickname() const { return nickname; }
+    std::string getPhoneNumber() const { return phoneNumber; }
+    std::string getDarkestSecret() const { return darkestSecret; }
+};
+
+#endif
