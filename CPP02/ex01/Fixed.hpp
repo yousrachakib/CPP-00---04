07e0421d046b5f3ff6f29a82aa8370db988d3f6a
Yousra <110408013+yousrachakib@users.noreply.github.com>
2023-10-22 17:49:38 +0100

    ex01 done

--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 17:06:41 by yochakib          #+#    #+#             */
-/*   Updated: 2023/10/20 12:35:00 by yochakib         ###   ########.fr       */
+/*   Updated: 2023/10/22 17:16:23 by yochakib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,38 @@
 
 
 #include <iostream>
+#include <cmath>
 
 
 class Fixed
 {
 private:
 	int 				fixed_point_value;
-	static const int	nu_fractionnal_bits = 8;
+	static const int	nu_fractionnal_bits;
 public:
+	// Constructor
 	Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
 	Fixed(const Fixed &copy);
+
+	// Destructor
 	~Fixed();
 
+	// Operation overloading
     Fixed& operator=(const Fixed& other);
+
+	//Method
+	int		toInt(void) const;
+	float	toFloat(void) const;
+
+	// getter
 	int getRawBits() const;
+
+	// setter
 	void setRawbits(int const raw);
 };
+std::ostream& operator<<(std::ostream& os,Fixed const &obj);
 
 
 #endif
